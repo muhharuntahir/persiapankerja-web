7f8f97fe05c8169580df8dfcffc4235a3a60238b
Muhammad Harun <muhharuntahir@gmail.com>
2025-12-13 14:24:36 +0800

    update history subscription

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -6,7 +6,10 @@ import * as schema from "./schema";
 
 // Supabase pooler harus disable prepare: false
 const client = postgres(process.env.DATABASE_URL!, {
-  prepare: false,
+  // prepare: false,
+  max: 1, // ⬅️ penting di Supabase Pooler
+  idle_timeout: 5,
+  connect_timeout: 10,
 });
 
 export const db = drizzle(client, { schema });
