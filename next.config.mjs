f02ca42c1f112f8ad83b612699a85aa9c803d9a8
Muhammad Harun <muhharuntahir@gmail.com>
2025-12-27 22:57:41 +0800

    math test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,40 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/(.*)",
+//         headers: [
+//           {
+//             key: "Access-Control-Allow-Origin",
+//             value: "*",
+//           },
+//           {
+//             key: "Access-Control-Allow-Methods",
+//             value: "GET, POST, PUT, DELETE, OPTIONS",
+//           },
+//           {
+//             key: "Access-Control-Allow-Headers",
+//             value: "Content-Type, Authorization",
+//           },
+//           {
+//             key: "Content-Range",
+//             value: "bytes : 0-9/*",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   async headers() {
     return [
       {
@@ -27,4 +62,9 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
